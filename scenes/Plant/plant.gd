bfda779f2ab45ba008fa4f68d089ecd38f473d5d
rBrink21 <brinkrrr@gmail.com>
2025-07-30 21:07:32 -0600

    done for tonight

--- scenes/Plant/plant.gd
+++ scenes/Plant/plant.gd
@@ -1,6 +1,7 @@
-extends StaticBody2D
+extends Node2D
 class_name Plant
 
+@export var activated_particle_effect : PackedScene
 
 
 func _onready() -> void: 
@@ -8,3 +9,7 @@ func _onready() -> void:
 
 func activate() -> void:
 	PointManager.increase_score(1)
+	var particle_effect = activated_particle_effect.instantiate()
+	add_child(particle_effect)
+	particle_effect.global_position = global_position
+	particle_effect.restart()
