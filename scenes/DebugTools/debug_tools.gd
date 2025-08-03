1a619974612b383cd6c1c5e901d890fb3805a28e
MasiEmanuele <manumasi901@gmail.com>
2025-08-03 14:22:49 +0200

    resoruce plant chang

--- scenes/DebugTools/debug_tools.gd
+++ scenes/DebugTools/debug_tools.gd
@@ -7,7 +7,7 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action("debug_gain_points"):
 		PointManager.points += 10
 	
-	if event.is_action("debug_test_plant_effect"):
+	if event.is_action_pressed("debug_test_plant_effect"):
 		var seed_spawner : SeedSpawner = get_tree().root.get_node("/root/Game/Washer").get_seed_spawner()
 		test_plant_data.effects = []
 		test_plant_data.effects.append(plant_effect)
