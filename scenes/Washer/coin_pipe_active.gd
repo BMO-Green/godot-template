599662833b991bc00cc468ca265b4071577cbfa2
vivikowski <vivi_lulkowski@gmx.de>
2025-08-03 09:56:53 +0200

    made the pipe blink when a coin goes through
    Inês!!! look!! code commit!!

---
+++ scenes/Washer/coin_pipe_active.gd
@@ -0,0 +1,18 @@
+extends Sprite2D
+
+
+func _ready() -> void:
+	self.visible = true 
+	self.modulate.a = 0.0
+	print(CurrencyManager.currency_spawner)
+	CurrencyManager.currency_spawner.on_coin_created.connect(coin_pipe_blink)
+	
+func coin_pipe_blink(useless_pointless_node_placeholder_coin : Node) -> void:
+	var tween : Tween
+	tween = create_tween()
+	tween.tween_property(self,"modulate:a", 1.0, 0.02)
+	tween.tween_interval(0.15)
+	tween.tween_property(self,"modulate:a", 0.0, 0.025)
+	tween.tween_property(self,"modulate:a", 1.0, 0.02)
+	tween.tween_interval(0.15)
+	tween.tween_property(self,"modulate:a", 0.0, 0.025)
