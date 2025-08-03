3a742bde8868cc88be9fcff10f6bcc59fbbbf8a0
Julien <julien.kaspar@googlemail.com>
2025-08-03 17:54:37 +0200

    Cleanup

--- scenes/Washer/door.gd
+++ scenes/Washer/door.gd
@@ -4,26 +4,13 @@ extends Node2D
 
 var tween : Tween
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	
 	if washer.is_spinning:
 		ongoing_rumble()
 	else:
 		position = Vector2.ZERO
-
-
-func create_rumble_tween() -> void:
-	
-	
-	
-	tween = create_tween()
-	#tween.tween_property(self, "position", )
 
 
 func ongoing_rumble() -> void:
@@ -37,5 +24,3 @@ func ongoing_rumble() -> void:
 	var y_sine := sin((elpased_time * speed) + PI*0.5)
 	
 	position = Vector2(x_sine, y_sine) * strength
-	
-	print(x_sine)
