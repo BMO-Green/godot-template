d04c2f7f86600cb41f7966927b764ce9900ef827
vivikowski <vivi_lulkowski@gmx.de>
2025-08-10 22:50:13 +0200

    undo of clamping of speed slider in the wrong spot
    gonna do it smart the next time!!

--- scripts/UI/speed_slider.gd
+++ scripts/UI/speed_slider.gd
@@ -9,7 +9,6 @@ func _ready() -> void:
 func _on_value_changed(new_value: float ) -> void:
 	
 	var spin_speed_index : int = remap(new_value, min_value, max_value, 0, MusicData.KEYS.size() - 1)
-	spin_speed_index = clamp(spin_speed_index, min_value, max_value)
 	MusicManager.set_music_key.emit(MusicData.KEYS[MusicData.KEYS.keys()[spin_speed_index]])
 	SfxManager.slider_sound.pitch_scale = remap(new_value, self.min_value, self.max_value, 0.5, 2.0)
 	
