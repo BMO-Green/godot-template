9d554f4bae90f0d646ab9665a5fe666acdeaad0a
rBrink21 <brinkrrr@gmail.com>
2025-08-11 12:35:37 -0600

    speed slider now more robust

--- scripts/UI/speed_slider.gd
+++ scripts/UI/speed_slider.gd
@@ -1,10 +1,12 @@
 extends HSlider
 
 @export var washer: Washer
-@export var speed_decay: float = 0.02
 
 func _ready() -> void:
 	washer.on_spin_speed_changed.connect(_on_value_changed)
+	min_value = washer.MIN_SPIN_SPEED
+	max_value = washer.MAX_SPIN_SPEED
+	value = washer.spin_speed
 
 func _on_value_changed(new_value: float ) -> void:
 	
