337fc84216a966411719d2740b3069a024301a13
rBrink21 <brinkrrr@gmail.com>
2025-11-16 20:18:10 +0100

    removing unstested effects and making planteffect a proper abstract class

--- scripts/PlantEffects/PlantEffect.gd
+++ scripts/PlantEffects/PlantEffect.gd
@@ -1,7 +1,5 @@
-class_name PlantEffect
+@abstract class_name PlantEffect
 extends Resource
 
-@warning_ignore("unused_parameter")
-func activate(plant: Plant):
-	push_error("PlantEffect.activate() not implemented in subclass")
+@abstract func activate(plant: Plant)
 	
